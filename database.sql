a4a2dce0e7bce87c5c1c57a794fbee4ef8da56fc
DionFinn <103545669@student.swin.edu.au>
2021-10-05 14:10:43 +1100

    Update database.sql
    TASK 3 COMPLETE

--- database.sql
+++ database.sql
@@ -87,7 +87,64 @@ CREATE TABLE ORDERLINE5669(
 
 SELECT * FROM  ORGANISATION5669
 SELECT * FROM  CLIENT5669
-SELECT * FROM  ORDER5559
+SELECT * FROM  ORDER5669
 SELECT * FROM MENUITEM5669
 SELECT * FROM  ORDERLINE5669
+
+
+DELETE
+FROM ORGANISATION5669
+
+DELETE 
+FROM CLIENT5669
+
+DELETE 
+FROM ORDER5669
+
+DELETE 
+FROM ORDERLINE5669
+
+DELETE
+FROM MENUITEM5669
+
+INSERT INTO ORGANISATION5669 (OrgID, OrganisationName) VALUES
+('DODG',	'Dod & Gy Widget Importers'),
+('SWUT',	'Swinburne University of Technology');
+
+INSERT INTO CLIENT5669 (ClientID, [Name], Phone ,OrgId) VALUES
+(12,	'James Hallinan',	'(03)5555-1234',	'SWUT'),
+(15,	'Any Nguyen',	'(03)5555-2345',	'DODG'),
+(18,	'Karen Mok',	'(03)5555-3456',	'SWUT'),
+(21,	'Tim Baird',	'(03)5555-4567',	'DODG');
+
+INSERT INTO ORDER5669 (ClientID, DateTimePlaced, DeliveryAddress) VALUES
+(12,	'2021-09-20',	'Room TB225 - SUT - 1 John Street, Hawthorn, 3122'),
+(21,	'2021-09-14',	'Room ATC009 - SUT - 1 John Street, Hawthorn, 3122'),
+(21,	'2021-09-27',	'Room TB225 - SUT - 1 John Street, Hawthorn, 3122'),
+(15,	'2021-09-20',	'The George - 1 John Street, Hawthorn, 3122'),
+(18,	'2021-09-30',	'Room TB225 - SUT - 1 John Street, Hawthorn, 3122');
+
+INSERT INTO MENUITEM5669 (ItemID, [Description], ServesPerUnit,	UnitPrice) VALUES
+(3214,	'Tropical Pizza - Large',	2,	 16.00),
+(3216,	'Tropical Pizza - Small',	1,	 12.00),
+(3218,	'Tropical Pizza - Family',	4,	 23.00),
+(4325,	'Can - Coke Zero',	1,	 2.50), 
+(4326,	'Can - Lemonade',	1,	 2.50), 
+(4327,	'Can - Harden Up',	1,	 7.50 );
+
+INSERT INTO ORDERLINE5669 (ItemId, ClientId, DateTimePlaced, Qty) VALUES 
+(3216,	12,	'2021-09-20',	2),
+(4326,	12,	'2021-09-20',	1),
+(3218,	21,	'2021-09-14',	1),
+(3214,	21,	'2021-09-14',	1),
+(4325,	21,	'2021-09-14',	4),
+(4327,	21,	'2021-09-14',	2),
+(3216,	21,	'2021-09-27',	1),
+(4327,	21,	'2021-09-27',	1),
+(3218,	21,	'2021-09-27',	2),
+(3216,	15,	'2021-09-20',	2),
+(4326,	15,	'2021-09-20',	1),
+(3216,	18,	'2021-09-30',	1),
+(4327,	18,	'2021-09-30',	1);
+
 
